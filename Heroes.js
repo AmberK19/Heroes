f91eb1375dd55c537b866e93f72fafe413561cbb
AmberK19 <kingamber746@gmail.com>
2025-02-13 23:44:23 -0600

    Add files via upload

---
+++ Heroes.js
@@ -0,0 +1,269 @@
+/**
+ * Decisions
+ */
+
+var isTrue= true
+
+var isFalse =false
+
+console.log(isTrue&&True);
+
+console.log(isTrue && isFalse); // false
+
+/**
+* if statement 
+*
+syntax:
+*
+*if (condition) {
+*    // do stuff
+*}
+*
+*the condition will evalute to either true or false
+*/
+*  understood "true"
+*/
+
+if (5>2){
+     console.log('five is greater than two');
+
+}
+
+/**
+ * if/else statement
+ * 
+ * syntax:
+ * 
+ * if (condition) {
+ * // do stuff 
+ * } else{
+ * // do something else 
+ *}
+ * 
+ * 
+ * var am Hungry = false
+ * 
+ * if(amHungry {
+    console.log 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var today = new Date ());
+
+console.log(today.getHours)
+
+if (today.getHours()<12{
+     console.log('Good morning')
+}else if (today.getHours()>=12 && today.getHours() < 17) {
+    console.log(Good afternoon');
+}else{
+console.log('Good evening'); 
+}
+
+console.clear()
+
+
+if( today.getDay()==0){
+    console.log('Sunday');
+}else
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Write a program that will determine a number is positive, number, or  zero
+ * 
+ * 
+ */Write a program that will determine if a number is odd,even or zero
+ */
+var Num=0
+if (number=0 )();
+ console.log number = zero
+ var number && zero is= false
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+/**
+ * Given a unit(either fah or cel)convert the tem perature. If the the unit is fah, conver the 
+    temp or cel.If the unit is cel,convert the temp to fah
+ *
+ */
+
+    var unit= 'fah'
+
+    var temp=212
+
+    varnewTemp
+
+
+    if 
+    
+
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <title>Heroes</title>
+    </head>
+    <body>
+    
+        <h1 class="text-center" id="heading">Heroes</h1>
+        <section class="section">
+            <div class="container">
+                <div class="mb-3">
+                    <form action="#" class="form">
+                        <button class="btn btn-info" id="loadBtn">Load Cards</button>
+                        <select name="filter" id="filter">
+                            <option value="">-- choose filter --</option>
+                            <option value="spiderman">Marvel</option>
+                            <option value="superman">DC</option>
+                            <option value="mirage">Mirage</option>
+                            <option value="image">Image</option>
+                            <option value="human">Human</option>
+                            <option value="wonderwoman">wonderwomman</option>
+                            <option value="batman">batman</option>
+                            <option value="myth">god</option>
+                        </select>
+                    </form>
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4" id="row"></div>
+            </div>
+        </section>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="js/app.js"></script>
+    </body>
+    </html>
